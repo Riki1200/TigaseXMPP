9ba6519a8d6c40555d4847d94ea61c8b6e40d65c
Romeo <45272844+Riki1200@users.noreply.github.com>
2024-04-24 04:26:34 -0400

    Update TigaseXMPP package and dependencies in Package.swift

--- Package.swift
+++ Package.swift
@@ -20,6 +20,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-     
+        .target(
+            name: "TigaseXMPP",
+            dependencies: [.product(name: "TigaseLogging", package: "tigase-logging.swift")]),
+        .testTarget(
+            name: "TigaseXMPPTests",
+            dependencies: ["TigaseXMPP"]),
     ]
 )
